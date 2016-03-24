88a00df3ef4dabd945989b4d4b05583f6bad9bc4
pkuyaoyao <525841634@qq.com>
2016-03-24 21:45:53 +0800

    Added files via upload

---
+++ charfreq.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python
+
+from __future__ import division  #using exact div
+import re
+import string
+
+freqDict = {}
+for word in string.lowercase:   #create a void freq dict, key=a to z
+    freqDict[word] = 0L
+
+fh=open('./a.txt','r')           #read_only
+str=string.lower(fh.read())
+for l in str:
+    if re.match("[a-z]",l):       #if l is a letter, count it
+        freqDict[l] += 1
+
+total = 0L
+for value in freqDict.values():
+    total += value
+
+list = sorted(freqDict.iteritems(),key=lambda t:t[1],reverse=True) #sort out
+for key,value in list:
+    print "%s %6d %6.3f%%" %(key,value,100*value/total)
+
+print "total = %d" %total
